2d42ffb9fc2bcb91ddd850ca5e1bad0733677860
Antony Omondi <tonymboya13@gmail.com>
2025-03-14 22:07:12 +0300

    complete Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+const random = Math.floor(Math.random()*10) + 1;
+console.log(random);
+
+const num3 = 19;
+const num4 = 5;
+
+const mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max(1,4,11,13,20,5,9,6,17,19,)
+console.log(max);
